c6bfc80c75373f10a84738297e0bb14e275a47e5
ne_srv <ne_srv@mail.ru>
2024-11-20 01:39:56 +0300

    autosave

--- DAY-2/docs/code.sql
+++ DAY-2/docs/code.sql
@@ -1,3 +1,28 @@
+-- 1
+CREATE TABLE books
+(
+    book_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title character varying(50),
+    authors text[],
+    price numeric(8, 2),
+    amount integer
+);
+
+ALTER TABLE IF EXISTS public.books
+    OWNER to student;
+
+
+-- 2
+
+INSERT INTO books(title, authors, price, amount)
+VALUES
+('Сказка о царе Салтане', (ARRAY['Пушкин А.С.']), 100, 10),
+('Муму', (ARRAY['Тургенев И.С']), 200, 8),
+('Трудно быть богом', (ARRAY['Стругацкий Б.Н., Стругацкий А.Н.']), 300,6),
+('Война и мир', (ARRAY['Толстой Л.Н']), 400, 4 ),
+('Братья Карамазовы', (ARRAY['Толстой Л.Н']), 500, 2 );
+
+
 ```sql
 INSERT INTO book (title, author, price, amount)
 VALUES
