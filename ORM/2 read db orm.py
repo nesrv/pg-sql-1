0edb0261fb4b0960e635c440bcdaf2e6a24c6397
nesrv <ne_srv@mail.ru>
2024-12-02 16:26:45 +0300

    autosave

--- ORM/2 read db orm.py
+++ ORM/2 read db orm.py
@@ -1,56 +1,54 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, Numeric, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import Session
 
 
-
+from models import Base, Author, Genre, Book
 
 engine = create_engine("sqlite:///ORM/books.db")
-
-class Base(DeclarativeBase): pass
-
-class Author(Base):
-    __tablename__ = "author"
-  
-    author_id = Column(Integer, primary_key=True, index=True)
-    name_author = Column(Text)
-    
-    def __repr__(self):
-        return f"Author(author_id={self.author_id}, name_author={self.name_author})"
-
-class Genre(Base):
-    __tablename__ = "genre"
-
-    genre_id = Column(Integer, primary_key=True, index=True)
-    name_genre = Column(Text)
-
-    def __repr__(self):
-        return f"Genre(genre_id={self.genre_id}, name_genre={self.name_genre})"   
-    
-class Book(Base):
-    __tablename__ = "book"
-
-    book_id = Column(Integer, primary_key=True, index=True)
-    title = Column(Text)
-    author_id = Column(Integer)
-    genre_id = Column(Integer)
-    price = Column(Numeric(10,2))
-    amount = Column(Integer)
-    author_id = Column(Integer, ForeignKey("author.id"))
-    genre_id = Column(Integer, ForeignKey("genre.id"))
-
-    def __repr__(self):
-        return f"Book(book_id={self.book_id}, title={self.title}, author_id={self.author_id}, genre_id={self.genre_id}, price={self.price}, amount={self.amount})"    
-    
-        
     
 Base.metadata.create_all(bind=engine)
 
-with Session(autoflush=False, bind=engine) as db:
-    authors = db.query(Author).all()
-    print(authors)
-    print('-'*20)
-    genres = db.query(Genre).all()
-    print(genres)
-    books = db.query(Book).all()
-    print(books)
+# простой вывод 
+def get_all_books():   
+    with Session(autoflush=False, bind=engine) as db:
+        authors = db.query(Author).all()
+        print(authors)
+        print('-'*20)
+        genres = db.query(Genre).all()
+        print(genres)
+        books = db.query(Book).all()
+        print(books)
 
+    
+
+
+# агрегирующие функции
+def get_aggregate():
+    with Session(autoflush=False, bind=engine) as db:
+        count = db.query(func.count(Book.book_id)).scalar()
+        print('Count books:', count)
+        max_price = db.query(func.max(Book.price)).scalar()
+        print('Maximum product price:', max_price)
+        min_price = db.query(func.min(Book.price)).scalar()
+        print('Minimum product price:', min_price)
+        sum_price = db.query(func.sum(Book.price * Book.amount)).scalar()
+        print('Sum price:', sum_price)
+        min_book = db.query(Book).filter(Book.price == min_price).first()
+        max_book = db.query(Book).filter(Book.price == max_price).first()
+        print(min_book)
+        print(max_book)
+        
+
+
+def get_join_tables():
+    with Session(autoflush=False, bind=engine) as db:
+        query = db.query(Book, Genre, Author)
+        # print (*query, end='\n')
+        for book, genre, author in query:
+            print(book, '|', genre, '|', author)
+    
+
+
+     
+get_aggregate()
+get_join_tables()
---
+++ ORM/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, Text, Numeric, ForeignKey, func
+from sqlalchemy.orm import DeclarativeBase, Session
+
+class Base(DeclarativeBase): pass
+
+class Author(Base):
+    __tablename__ = "author"
+  
+    author_id = Column(Integer, primary_key=True, index=True)
+    name_author = Column(Text)
+    
+    def __repr__(self):
+        return f"Author(author_id={self.author_id}, name_author={self.name_author})"
+
+class Genre(Base):
+    __tablename__ = "genre"
+
+    genre_id = Column(Integer, primary_key=True, index=True)
+    name_genre = Column(Text)
+
+    def __repr__(self):
+        return f"Genre(genre_id={self.genre_id}, name_genre={self.name_genre})"   
+    
+class Book(Base):
+    __tablename__ = "book"
+
+    book_id = Column(Integer, primary_key=True, index=True)
+    title = Column(Text)
+    author_id = Column(Integer)
+    genre_id = Column(Integer)
+    price = Column(Numeric(10,2))
+    amount = Column(Integer)
+    author_id = Column(Integer, ForeignKey("author.id"))
+    genre_id = Column(Integer, ForeignKey("genre.id"))
+
+    def __repr__(self):
+        return f"Book(book_id={self.book_id}, title={self.title}, author_id={self.author_id}, genre_id={self.genre_id}, price={self.price}, amount={self.amount})"    
+    
