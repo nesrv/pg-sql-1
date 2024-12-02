03ad4ad5862c54da75bd55c45f740d44597530f1
nesrv <ne_srv@mail.ru>
2024-12-03 00:43:18 +0300

    autosave

--- ORM/5 tasks.py
+++ ORM/5 tasks.py
@@ -8,16 +8,32 @@
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import Session
 
-from models import Book, Supply
+from models import Book, Supply, Base
 
-engine = create_engine("sqlite:///ORM/books.db")
+engine = create_engine("sqlite:///ORM/books.db", echo=True)
+# Base.metadata.create_all(bind=engine)
     
 def get_book_join_supply():
     with Session(autoflush=False, bind=engine) as db:
         query = db.query(Book, Supply).filter(Book.price == Supply.price).all()
-        # print(query)
-        for book in query:
-            print(book.Book.title, book.Supply.price) 
-    
+        test1 = db.query(Book).filter(Book.title=="Мастер и Маргарита").first()
+        test1.amount += 1
+        db.commit()
+        print(test1)
+        
+        
+        
+        for book, supply in query:
+            # current_book = db.query(Book).filter(Book.book_id == book.book_id).first()
+            # print(current_book)
+            # current_book.amount += supply.amount
+            print(book.title, book.amount,supply.amount) 
+            # book.amount += supply.amount
+            # print(book.title, book.amount,supply.amount) 
+            # current_book.amount +=supply.amount
+            # book.Supply.amount = 0
+            
+        # db.commit()
+        
      
 get_book_join_supply()
